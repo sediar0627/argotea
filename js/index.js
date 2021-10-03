6afea4422f20b52901c6b9ef3f20cf164d5cfd69
sediar0627 <=>
2021-10-03 14:04:00 -0500

    ajuste menu

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $(".link_lista_bebidas").click(function(e){
+        window.location.href = "lista_bebidas.html";
+    })
+
+    $(".link_lista_deseos").click(function(e){
+        window.location.href = "lista_deseos.html";
+    })
+
+    $(".link_realizar_pedido").click(function(e){
+        window.location.href = "realizar_pedido.html";
+    })
+    
+})
--- js/sb-admin-2.js
+++ js/sb-admin-2.js
@@ -1,24 +1,39 @@
-(function($) {
+
+let side_nav_status = localStorage.getItem("side-nav");
+if (side_nav_status) {
+  if (side_nav_status == "cerrado") {
+    $('.sidebar .collapse').collapse('hide');
+    $(".nav-link-text").hide();
+    $("body").toggleClass("sidebar-toggled");
+    $(".sidebar").toggleClass("toggled");
+  }
+}
+
+(function ($) {
   "use strict"; // Start of use strict
 
   // Toggle the side navigation
-  $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
+  $("#sidebarToggle, #sidebarToggleTop").on('click', function (e) {
     $("body").toggleClass("sidebar-toggled");
     $(".sidebar").toggleClass("toggled");
     if ($(".sidebar").hasClass("toggled")) {
       $('.sidebar .collapse').collapse('hide');
       $(".nav-link-text").hide();
-    }else{
+      localStorage.removeItem("side-nav");
+      localStorage.setItem("side-nav", "cerrado");
+    } else {
       $(".nav-link-text").show();
+      localStorage.removeItem("side-nav");
+      localStorage.setItem("side-nav", "abierto");
     }
   });
 
   // Close any open menu accordions when window is resized below 768px
-  $(window).resize(function() {
+  $(window).resize(function () {
     if ($(window).width() < 768) {
       $('.sidebar .collapse').collapse('hide');
     };
-    
+
     // Toggle the side navigation when window is resized below 480px
     if ($(window).width() < 480 && !$(".sidebar").hasClass("toggled")) {
       $("body").addClass("sidebar-toggled");
@@ -28,7 +43,7 @@
   });
 
   // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
-  $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
+  $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function (e) {
     if ($(window).width() > 768) {
       var e0 = e.originalEvent,
         delta = e0.wheelDelta || -e0.detail;
@@ -38,7 +53,7 @@
   });
 
   // Scroll to top button appear
-  $(document).on('scroll', function() {
+  $(document).on('scroll', function () {
     var scrollDistance = $(this).scrollTop();
     if (scrollDistance > 100) {
       $('.scroll-to-top').fadeIn();
@@ -48,7 +63,7 @@
   });
 
   // Smooth scrolling using jQuery easing
-  $(document).on('click', 'a.scroll-to-top', function(e) {
+  $(document).on('click', 'a.scroll-to-top', function (e) {
     var $anchor = $(this);
     $('html, body').stop().animate({
       scrollTop: ($($anchor.attr('href')).offset().top)
